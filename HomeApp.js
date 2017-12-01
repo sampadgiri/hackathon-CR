a372cdbf81bf3958d977f87a802e4518d51bdf64
sampadgiri <sampad.giri@gmail.com>
2017-12-02 02:38:58 +0530

    Add files via upload
    Logistics Admin dashboard - initial version

---
+++ HomeApp.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+var App = angular.module('App', []);
+App.controller('MainCtrl', function($scope, $http) {
+	$http.get('OrderDtls.json')
+	   .then(function(res){
+		  $scope.OrderDtls = res.data;                
+		});
+	$http.get('CarrDtls.json')
+	   .then(function(res){
+		  $scope.CarrDtls = res.data;                
+		});
+	$scope.getStars = function(rating){
+		var val = parseFloat(rating);
+		var size = val/5*100;
+		return size + '%';
+		}	
+});
+})();
