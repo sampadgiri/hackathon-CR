ea27fdd0503dc27366c70ecac45488a1f1f2a495
sampadgiri <sampad.giri@gmail.com>
2017-12-05 03:07:08 +0530

    Add files via upload
    Changed logic for carrier ratings to be city based

--- HomeApp.js
+++ HomeApp.js
@@ -14,6 +14,19 @@
 			var val = parseFloat(rating);
 			var size = val/5*100;
 			return size + '%';
-			}		
+			}
+	});
+	App.filter('unique', function() {
+		return function(input, key) {
+			var unique = {};
+			var uniqueList = [];
+			for(var i = 0; i < input.length; i++){
+				if(typeof unique[input[i][key]] == "undefined"){
+					unique[input[i][key]] = "";
+					uniqueList.push(input[i]);
+				}
+			}
+			return uniqueList;
+		};
 	});
 })();
