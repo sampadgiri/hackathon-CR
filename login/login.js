f57242a7b4cdeedaee61a38074b976e68fc6523d
radharani-mishra <34267398+radharani-mishra@users.noreply.github.com>
2017-12-06 14:28:58 +0530

    login related changes

---
+++ login/login.js
@@ -0,0 +1,34 @@
+dodApp.controller('loginCtrl', function ($scope, $http, $window) {
+    $http.get('login/login.json').then(function (res) {
+        $scope.loginDetails = res.data;
+    })
+    $scope.validate = function (username, password) {
+        var isValidated = false;
+        angular.forEach($scope.loginDetails, function (eachOption, eachValue) {
+            var userDetails={};
+            if (eachOption.userName == username && eachOption.password == password) {
+                isValidated = true;
+
+                if (eachOption.role == 'admin') {
+                    $window.location.href = '#!/admin'
+                } else {
+                    sessionStorage.setItem('userDetails',JSON.stringify(username));
+                    if (eachOption.priority == 'prime') {
+                        $window.location.href = '#!/user'
+
+
+                    } else {
+                        $window.location.href = '#!/user'
+
+
+                    }
+                }
+            }
+            if (isValidated = false) {
+                $window.location.href = '#!/login'
+
+            }
+        })
+    }
+});
+
