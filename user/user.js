862cbdbc759d8ff033a5a7f96fc2940f5c1b324c
radharani-mishra <34267398+radharani-mishra@users.noreply.github.com>
2017-12-07 12:25:10 +0530

    user page updates

--- user/user.js
+++ user/user.js
@@ -1,14 +1,18 @@
-dodApp.controller('userCtrl',function($scope,$http){
+dodApp.controller('userCtrl',function($scope,$http,$window){
 
     $http.get('user/user.json').then(function (res) {
         $scope.productsToBuy = res.data;
     });
-    $scope.userName = JSON.parse(sessionStorage.getItem('userDetails'));
+    $scope.userName = JSON.parse(sessionStorage.getItem('userName'));
+ $scope.userPriority = JSON.parse(sessionStorage.getItem('userPriority'));
+
      $scope.switchLocation = function () {
           $scope.location = $scope.selected;
      }
      $scope.submitReq = function () {
          console.log($scope.userName);
          console.log($scope.location);
+         console.log($scope.userPriority);
+         $window.location.href = '#!/success'
      }
 })
