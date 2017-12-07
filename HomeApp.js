431f08a4de49190bd1211bbb0b0305613ae9dae7
amritapritam442 <amrita.pritam.4@gmail.com>
2017-12-07 08:29:00 +0530

    Pagination added

--- HomeApp.js
+++ HomeApp.js
@@ -1,7 +1,8 @@
 (function() {
 	'use strict';
-	var App = angular.module('App', []);
+	var App = angular.module('App', ['angularUtils.directives.dirPagination']);
 	App.controller('MainCtrl', function($scope, $http) {
+	$scope.currentPage = 1;
 		$http.get('OrderDtls.json')
 		   .then(function(res){
 			  $scope.OrderDtls = res.data;                
@@ -9,7 +10,9 @@
 		$http.get('CarrDtls.json')
 		   .then(function(res){
 			  $scope.CarrDtls = res.data; 
-			  $scope.filteredCarrDtls=angular.copy($scope.CarrDtls);
+			  //$scope.onCitySelect($scope.CarrDtls[1].City);
+			 // $scope.CityVal=$scope.CarrDtls[1];
+			  //$scope.filteredCarrDtls=angular.copy($scope.CarrDtls);
 			});
 		$scope.getStars = function(rating){
 			var val = parseFloat(rating);
@@ -33,10 +36,7 @@
 			
 		$scope.onCitySelect=function(city){
 			var carrDetailsForCity=[];
-			if(city===""){
-			$scope.filteredCarrDtls=angular.copy($scope.CarrDtls);
-			}
-			else{
+			
 			angular.forEach($scope.CarrDtls, function(obj){
     
                 if(obj.City===city){
@@ -44,11 +44,19 @@
                        }
                  });
 			$scope.filteredCarrDtls=carrDetailsForCity;
-			}
+			
 	         // return carrDetailsForCity;		
 			};
+		$scope.changeCarrier=function(CarrVal,OrderDtl){
+		$http.post('/ChangeCarrier',{OrderNo:OrderDtl.OrderNo,Carrier:CarrVal})
+		   .then(function(res){
+			  alert(res.data.status); 
+			  //$scope.onCitySelect($scope.CarrDtls[1].City);
+			 // $scope.CityVal=$scope.CarrDtls[1];
+			  //$scope.filteredCarrDtls=angular.copy($scope.CarrDtls);
+			});
+		}	
 			
-			$scope.filteredCarrDtls=angular.copy($scope.CarrDtls);
 	});
 	App.filter('unique', function() {
 		return function(input, key) {
