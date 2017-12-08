8aa36a3eb62ef80f69ef3b1e786f503e9a17fc27
radharani-mishra <34267398+radharani-mishra@users.noreply.github.com>
2017-12-08 14:54:16 +0530

    Minor changes

--- user/user.js
+++ user/user.js
@@ -13,6 +13,20 @@ dodApp.controller('userCtrl',function($scope,$http,$window){
          console.log($scope.userName);
          console.log($scope.location);
          console.log($scope.userPriority);
+        //  var data = {};
+        //  dataObj.userName = $scope.userName;
+        //  dataObj.location = $scope.location;
+        //  dataObj.userPriority = $scope.userPriority;
+        //  var response = $http.post(url,dataObj);
+        //  response.success(function (data,status,headers,config) {
+        //               $window.location.href = '#!/success'
+
+        //  });
+        //  response.error(function (data,status,headers,config) {
+        //               $window.location.href = '#!/failure';
+
+        //  });
          $window.location.href = '#!/success'
+        // $window.location.href = '#!/failure';
      }
 })
