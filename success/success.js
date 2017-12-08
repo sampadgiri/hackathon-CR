496a052062b173112ccd017804c00a16a2403e35
radharani-mishra <34267398+radharani-mishra@users.noreply.github.com>
2017-12-08 20:31:10 +0530

    Minor changes

--- success/success.js
+++ success/success.js
@@ -1,5 +1,8 @@
 dodApp.controller('successCtrl',function($scope,$http,$window){
         $scope.userName = JSON.parse(sessionStorage.getItem('userName'));
+        $scope.orderId = sessionStorage.getItem('orderIdDetais');
+        console.log($scope.orderId);
+
 
 
      $scope.goToInbox = function () {
